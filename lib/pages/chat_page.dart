b36ba74028619c21388f2378468834f04f2f8ca3
Kenzie <ktebenezer2005@gmail.com>
2025-12-03 10:18:35 +0700

    change to Gemini

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -13,7 +13,23 @@ class _ChatPageState extends State<ChatPage> {
 
   final List<Message> _messages = [];
   final TextEditingController _controller = TextEditingController();
-  final openAI = AIService();
+  final AI = AIService();
+
+  void _sendMessage() async {
+    final text = _controller.text;
+    if(text.isEmpty) return;
+
+    setState(() {
+      _messages.add(Message(text, true));
+    });
+    _controller.clear();
+
+    final reply = await AI.generateMessage(text);
+
+    setState(() {
+      _messages.add(Message(reply, false));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +37,8 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO
           (0, 126, 160, 1),
-        title: const Text('Chat OpenAI',
-          style: TextStyle(color: Colors.black),
+        title: const Text('Chat AI',
+          style: TextStyle(color: Colors.white),
         ),
       ),
       body: Column(
@@ -31,9 +47,34 @@ class _ChatPageState extends State<ChatPage> {
             itemCount: _messages.length,
             itemBuilder: (_, i){
               final msg = _messages[i];
-              return Container();
+              return Container(
+                alignment: msg.status ? Alignment.centerRight : Alignment.centerLeft,
+                padding: EdgeInsets.all(8),
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: msg.status ? Colors.blue : Colors.grey,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Text(msg.text, style: TextStyle(
+                      color: msg.status ? Colors.white : Colors.black)
+                  ),
+                ),
+              );
             },
-          ))
+          )),
+          Row(
+            children: [
+              Expanded(child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                    hintText: "Type a Message...",
+                    contentPadding: EdgeInsets.all(12)
+                ),
+              )),
+              IconButton(onPressed: _sendMessage, icon: Icon(Icons.send))
+            ],
+          )
         ],
       )
     );
--- lib/service/ai_service.dart
+++ lib/service/ai_service.dart
@@ -1,30 +1,17 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:google_generative_ai/google_generative_ai.dart';
 
 class AIService {
-  final String API_KEY = "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";
+  final String API_KEY = "API";
 
-  Future<String> generateMessage(String message) async {
-    final url = Uri.parse("https://api.openai.com/v1/chat/completions");
+  late final GenerativeModel model;
 
-    final response = await http.post(
-      url,
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer $API_KEY",
-      },
-      body: jsonEncode({
-        "model": 'gpt-4o-mini',
-        "message": message,
-        "max token": 100,
-      }),
-    );
+  AIService(){
+    model = GenerativeModel(model: "gemini-2.5-flash", apiKey: API_KEY);
+  }
 
-    if(response.statusCode == 200){
-      final data = jsonDecode(response.body);
-      return data['choices'][0]['text'].toString().trim();
-    } else {
-      throw Exception('failed to load OpenAI response: ${response.body}');
-    }
+  Future<String> generateMessage(String message) async{
+    final content = [Content.text(message)];
+    final response = await model.generateContent(content);
+    return response.text ?? "No Response";
   }
 }
